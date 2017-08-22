e995f94175fed76d87a4a5d6b331c776fe04e330
juliomixco <julio.mixg@gmail.com>
2017-08-21 18:47:53 -0600

    new changes

---
+++ WebApplication1.Tests/Controllers/InstitutionControllerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1;
+using WebApplication1.Controllers;
+using WebApplication1.Services;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class InstitutionControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            InstitutionController controller = new InstitutionController(new InstitutionService(), new OrgService());
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Institution model = result.Model as Institution;
+            Assert.AreEqual("Sample Institution", model.Name);
+
+        }
+
+        [TestMethod]
+        public void Index2_should_use_OrgService_instance()
+        {
+            // Arrange
+            InstitutionController controller = new InstitutionController(new InstitutionService(), new OrgService());
+
+            // Act
+            ViewResult result = controller.Index2() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Institution model = result.Model as Institution;
+            Assert.AreEqual("ORG !!!! Institution", model.Name);
+
+        }
+
+    }
+}
