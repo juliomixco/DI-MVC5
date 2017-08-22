5040f7d42826c219c656ac333a9aaf363fc10ba3
juliomixco <julio.mixg@gmail.com>
2017-08-21 18:59:08 -0600

    update

--- WebApplication1.Tests/Controllers/InstitutionControllerTest.cs
+++ WebApplication1.Tests/Controllers/InstitutionControllerTest.cs
@@ -8,17 +8,38 @@ using WebApplication1;
 using WebApplication1.Controllers;
 using WebApplication1.Services;
 using WebApplication1.Entities;
+using Microsoft.Practices.Unity;
 
 namespace WebApplication1.Tests.Controllers
 {
     [TestClass]
     public class InstitutionControllerTest
     {
+        private InstitutionController getInstitutionControllerInstance() {
+            var container = new UnityContainer();
+
+            // register all your components with the container here
+            // it is NOT necessary to register your controllers
+
+            // e.g. container.RegisterType<ITestService, TestService>();
+
+            container.RegisterType<ICourseService, CourseService>();
+            container.RegisterType<IInstitutionService, InstitutionService>("institution");
+            container.RegisterType<IInstitutionService, OrgService>("org");
+
+            container.RegisterType<InstitutionController>(
+                new InjectionConstructor(
+                    new ResolvedParameter<IInstitutionService>("institution"),
+                new ResolvedParameter<IInstitutionService>("org")
+                ));
+
+            return container.Resolve<InstitutionController>();
+        } 
         [TestMethod]
         public void Index()
         {
             // Arrange
-            InstitutionController controller = new InstitutionController(new InstitutionService(), new OrgService());
+            InstitutionController controller = getInstitutionControllerInstance();
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
@@ -34,7 +55,7 @@ namespace WebApplication1.Tests.Controllers
         public void Index2_should_use_OrgService_instance()
         {
             // Arrange
-            InstitutionController controller = new InstitutionController(new InstitutionService(), new OrgService());
+            InstitutionController controller = getInstitutionControllerInstance();
 
             // Act
             ViewResult result = controller.Index2() as ViewResult;
